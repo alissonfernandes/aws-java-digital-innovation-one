bef7fe1c4224bd8cac805ed0259451a59b2c3a69
Alisson Fernandes <alissonfernandes200@gmail.com>
2021-10-27 21:36:24 -0300

    Create a function AWS Lambda

---
+++ src/main/java/com/serverless/rest/Function01.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.serverless.rest;
+
+/**
+ *
+ * @author User
+ */
+public class Rest {
+    
+}
