f33b6ec8122c6b2d30953c127e5ca5c42d0b1824
Alisson Fernandes <alissonfernandes200@gmail.com>
2021-10-27 21:38:05 -0300

    Create a function AWS Lambda

--- src/main/java/com/serverless/rest/Function01.java
+++ src/main/java/com/serverless/rest/Function01.java
@@ -1,14 +1,33 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.serverless.rest;
 
-/**
- *
- * @author User
- */
-public class Rest {
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+import com.google.gson.JsonObject;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+
+public class Function01 implements RequestStreamHandler {
+
+    @Override
+    public void handleRequest(InputStream in, OutputStream out, Context cntxt) throws IOException {
+        
+        JsonObject responseJson = new JsonObject();
+        JsonObject responseBody = new JsonObject();
+        JsonObject headerJson = new JsonObject();
+        
+        responseBody.addProperty("message", "OK here");
+        headerJson.addProperty("value", "value");
+        
+        //Http status code
+        responseJson.addProperty("statusCode", "200");
+        responseJson.add("headers", headerJson);
+        responseJson.addProperty("body", responseBody.toString());
+        
+        OutputStreamWriter writer = new OutputStreamWriter(out, "UTF-8");
+        writer.write(responseJson.toString());
+        writer.close();
+    }
     
 }
